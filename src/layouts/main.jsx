d58f6094ed2ac0a26137bf84ef364ec11ea12586
happy436 <lelouch436@gmail.com>
2022-06-04 10:17:25 +0300

    firebase database

--- src/layouts/main.jsx
+++ src/layouts/main.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { ProgressBar } from "react-bootstrap";
+import useMockData from "./../utils/mockData";
 
 const Main = () => {
-    return (<div>Main</div>);
+    const { error, initialize, progress, status } = useMockData();
+    function handleClick() {
+        initialize();
+    }
+    return (
+        <div className="container mt-5">
+            <h1>Main</h1>
+            <h3>Initialization data on firebase</h3>
+            <ul>
+                <li>Status: {status}</li>
+                <li>Progress: {progress}</li>
+                <li><ProgressBar now={progress} label={`${progress}%`} /></li>
+                {error && <li>Error: {error}</li>}
+            </ul>
+            <button className="btn btn-primary" onClick={handleClick}>
+                Init
+            </button>
+        </div>
+    );
 };
 
 export default Main;
--- src/services/http.services.js
+++ src/services/http.services.js
@@ -1,11 +1,35 @@
 import axios from "axios";
 import { toast } from "react-toastify";
-import config from "../config.json";
+import configFile from "../config.json";
 
-axios.defaults.baseURL = config.apiEndPoint;
+axios.defaults.baseURL = configFile.apiEndPoint;
+
+function transformData(data) {
+    return data ? Object.keys(data).map((key) => ({ ...data[key] })) : [];
+}
+
+axios.interceptors.request.use(
+    function (config) {
+        if (configFile.isFirebase) {
+            const containeSlash = /\/$/gi.test(config.user);
+            config.url =
+                (containeSlash ? config.url.slice(0, -1) : config.url) +
+                ".json";
+        }
+        return config;
+    },
+    function (error) {
+        return Promise.reject(error);
+    }
+);
 
 axios.interceptors.response.use(
-    (res) => res,
+    (res) => {
+        if (configFile.isFirebase) {
+            res.data = { content: transformData(res.data) };
+        }
+        return res;
+    },
     function (error) {
         const expectedErrors =
             error.response &&
